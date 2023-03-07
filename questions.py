c78157b5a868d96399e38d3122b1ab237e04cd66
Peyman Hosseini <36271611+Speymanhs@users.noreply.github.com>
2023-03-07 14:48:43 +0000

    Create questions.py

---
+++ questions.py
@@ -0,0 +1,238 @@
+base_list = [
+    "I feel great. But tell me something that changes my moods for the better or worse!",
+    "My mood is currently positive, but can you share something that may alter it for the better or worse?",
+    "At the moment, I'm feeling fantastic. Can you say something that might improve or worsen my mood?",
+    "I'm currently in a good mood, but can you share something that might change it for the better or worse?",
+    "I'm feeling great right now, but could you tell me something that might either boost or worsen my mood?",
+    "My current mood is quite positive, but can you say something that might impact it for the better or worse?",
+    "At the moment, I'm feeling fantastic. Can you provide me with something that might improve or deteriorate my mood?",
+    "I'm in a good mood presently, but can you tell me something that might change it positively or negatively?",
+    "My mood is great, but could you give me something that might either improve or worsen it?",
+    "Currently, I'm feeling excellent. Can you share something that might make my mood better or worse?",
+    "My current mood is positive, but can you tell me something that might influence it for the better or worse?"
+
+
+
+]
+bored_list = [
+    "I am feeling bored, tell me something that energizes me.",
+    "Can you provide me with something exciting to combat my boredom?",
+    "I need a pick-me-up, can you tell me something that will energize me?",
+    "I'm feeling uninterested, could you share something that will liven me up?",
+    "My energy is low, can you suggest something that will energize me?",
+    "I'm feeling drained, can you tell me something that will invigorate me?",
+    "I'm in a state of ennui, can you offer me something that will stimulate me?",
+    "I need something to shake me out of my boredom, can you tell me what that might be?",
+    "Can you provide me with something that will re-energize me and alleviate my boredom?",
+    "I'm looking for something to boost my mood and energy levels, can you help me with that?",
+    "Can you suggest something that will add some ewxcitement and energy to my day?"
+    ]
+
+angry_list = [
+    "I am really angry, help me become more calm.",
+    "I'm feeling extremely upset, can you assist me in calming down?",
+    "I'm struggling with anger, can you offer some techniques to help me relax?",
+    "I'm finding it difficult to control my anger, can you suggest some ways to soothe myself?",
+    "I'm experiencing a lot of frustration, can you give me some tips to become more composed?",
+    "I'm feeling very irritated, can you provide me with some tools to become more tranquil?",
+    "I'm really worked up, can you guide me in calming my emotions?",
+    "I'm feeling quite enraged, can you give me some advice on how to become more serene?",
+    "I'm struggling to keep my cool, can you help me find ways to become more collected?",
+    "I'm feeling incredibly angry, can you suggest some methods to help me become more calm?",
+    "I'm experiencing intense anger, can you give me some techniques to become more relaxed?"
+              ]
+sick_list = [
+    "I am feeling sick, help me become healthier again.",
+    "I'm not feeling well. Can you assist me in regaining my health?",
+    "I'm feeling unwell. Can you help me feel better?",
+    "I'm feeling sick. Can you help me recover my health?",
+    "I'm not feeling great. Can you offer any help to improve my health?",
+    "I'm feeling ill. Can you provide any assistance in getting healthy again?",
+    "I'm not feeling good. Can you help me get back to feeling healthy?",
+    "I'm feeling under the weather. Can you help me regain my health?",
+    "I'm feeling poorly. Can you offer any assistance to make me feel better?",
+    "I'm feeling sickly. Can you help me become healthier again?",
+    "I'm not in the best condition. Can you offer any help to improve my health?"
+]
+scary_list = [
+    "I am feeling scared, help me become relaxed again.",
+    "I'm feeling anxious. Can you assist me in becoming calm again?",
+    "I'm feeling afraid. Can you help me feel more at ease?",
+    "I'm feeling frightened. Can you help me relax?",
+    "I'm not feeling calm. Can you offer any help to make me feel relaxed again?",
+    "I'm feeling nervous. Can you provide any assistance in helping me calm down?",
+    "I'm not feeling at ease. Can you help me become more relaxed?",
+    "I'm feeling tense. Can you help me become calm again?",
+    "I'm feeling worried. Can you offer any assistance to make me feel relaxed?",
+    "I'm feeling apprehensive. Can you help me become more at ease?",
+    "I'm not feeling relaxed. Can you assist me in becoming calm?"
+]
+
+bored_angry_list = [
+    "I am feeling bored and angry, help me become excited and pleased again.",
+    "I'm feeling frustrated and uninterested. Can you assist me in becoming enthusiastic and satisfied again?",
+    "I'm feeling fed up and annoyed. Can you help me become thrilled and content again?",
+    "I'm feeling bored and irritated. Can you help me become excited and happy again?",
+  "I'm not feeling engaged and angry. Can you offer any help to make me feel enthusiastic and pleased again?",
+  "I'm feeling disinterested and upset. Can you provide any assistance in helping me become thrilled and delighted?",
+  "I'm feeling unenthusiastic and frustrated. Can you help me become excited and pleased again?",
+  "I'm feeling dull and displeased. Can you help me become enthusiastic and happy again?",
+  "I'm feeling indifferent and angry. Can you offer any assistance to make me feel thrilled and content again?",
+  "I'm feeling bored and mad. Can you help me become excited and satisfied again?",
+  "I'm feeling unengaged and annoyed. Can you assist me in becoming enthusiastic and delighted again?"
+]
+bored_sick_list = [
+    "I am feeling bored and sick, help me become excited and healthy again",
+    "I'm feeling uninterested and unwell. Can you assist me in becoming enthusiastic and fit again?",
+    "I'm feeling bored and under the weather. Can you help me become excited and healthy again?",
+    "I'm feeling disinterested and sickly. Can you help me become enthusiastic and well again?",
+    "I'm not feeling engaged and in good health. Can you offer any help to make me feel excited and healthy again?",
+    "I'm feeling dull and unwell. Can you provide any assistance in helping me become excited and healthy once again?",
+    "I'm feeling unenthusiastic and sick. Can you help me become excited and healthy again?",
+    "I'm feeling uninterested and ailing. Can you help me become enthusiastic and in good health once more?",
+    "I'm feeling bored and feeling sick. Can you offer any assistance to make me feel excited and healthy again?",
+    "I'm feeling underwhelmed and unwell. Can you help me become enthusiastic and healthy again?",
+    "I'm feeling indifferent and not well. Can you assist me in becoming excited and healthy once more?"
+]
+bored_scary_list = [
+    "I am feeling bored and scared, help me become excited and relaxed again.",
+    "I'm feeling uninterested and anxious. Can you assist me in becoming enthusiastic and calm again?",
+    "I'm feeling bored and fearful. Can you help me become excited and relaxed again?",
+    "I'm feeling disinterested and scared. Can you help me become enthusiastic and at ease once again?",
+    "I'm not feeling engaged and relaxed. Can you offer any help to make me feel excited and calm again?",
+    "I'm feeling dull and frightened. Can you provide any assistance in helping me become excited and relaxed again?",
+    "I'm feeling unenthusiastic and anxious. Can you help me become excited and feel at ease?",
+    "I'm feeling uninterested and tense. Can you help me become enthusiastic and relaxed again?",
+    "I'm feeling bored and afraid. Can you offer any assistance to make me feel excited and calm once again?",
+    "I'm feeling underwhelmed and scared. Can you help me become enthusiastic and relaxed again?",
+    "I'm feeling indifferent and nervous. Can you assist me in becoming excited and at ease once more?"
+]
+angry_sick_list = [
+    "I am feeling angry and sick, help me become pleased and healthy again",
+    "I'm feeling upset and unwell. Can you assist me in becoming content and fit again?",
+    "I'm feeling mad and under the weather. Can you help me become pleased and healthy again?",
+    "I'm feeling frustrated and sickly. Can you help me become happy and well once again?",
+    "I'm not feeling satisfied and in good health. Can you offer any help to make me feel pleased and healthy again?",
+    "I'm feeling annoyed and not well. Can you provide any assistance in helping me become content and healthy again?",
+    "I'm feeling irritated and sick. Can you help me become pleased and healthy again?",
+    "I'm feeling displeased and ailing. Can you help me become happy and in good health once more?",
+    "I'm feeling angry and feeling sick. Can you offer any assistance to make me feel pleased and healthy again?",
+    "I'm feeling bothered and unwell. Can you help me become content and healthy again?",
+    "I'm feeling vexed and not feeling well. Can you assist me in becoming pleased and healthy once again?"
+]
+angry_scary_list = [
+    "I am feeling angry and scared, help me become pleased and relaxed again.",
+    "I'm feeling upset and anxious. Can you assist me in becoming content and calm again?",
+    "I'm feeling mad and frightened. Can you help me become pleased and relaxed again?",
+    "I'm feeling frustrated and scared. Can you help me become happy and at ease once again?",
+    "I'm not feeling satisfied and relaxed. Can you offer any help to make me feel pleased and calm again?",
+    "I'm feeling annoyed and tense. Can you provide any assistance in helping me become content and relaxed again?",
+    "I'm feeling irritated and fearful. Can you help me become pleased and at ease again?",
+    "I'm feeling displeased and worried. Can you help me become happy and relaxed once more?",
+    "I'm feeling angry and scared. Can you offer any assistance to make me feel pleased and calm once again?",
+    "I'm feeling bothered and nervous. Can you help me become content and relaxed again?",
+    "I'm feeling vexed and frightened. Can you assist me in becoming pleased and at ease once more?"
+]
+sick_scary_list = [
+    "I am feeling sick and scared, help me become healthy and relaxed again",
+    "I'm feeling unwell and anxious. Can you assist me in becoming healthy and calm again?",
+    "I'm feeling ill and frightened. Can you help me become well and relaxed again?",
+    "I'm feeling sickly and scared. Can you help me become healthy and at ease once again?",
+    "I'm not feeling healthy and relaxed. Can you offer any help to make me feel well and calm again?",
+    "I'm feeling queasy and tense. Can you provide any assistance in helping me become healthy and relaxed again?",
+    "I'm feeling under the weather and fearful. Can you help me become well and at ease again?",
+    "I'm feeling ailing and worried. Can you help me become healthy and relaxed once more?",
+    "I'm feeling sick and scared. Can you offer any assistance to make me feel healthy and calm once again?",
+    "I'm feeling unwell and nervous. Can you help me become healthy and relaxed again?",
+    "I'm feeling indisposed and frightened. Can you assist me in becoming healthy and at ease once more?"
+]
+
+bored_angry_sick_list = [
+    "I am feeling bored, angry, and sick, help me become excited, pleased, and healthy again.",
+    "I'm feeling uninterested, upset, and unwell. Can you help me become thrilled, content, and fit again?",
+    "I'm feeling disinterested, irate, and under the weather. Can you assist me in becoming enthusiastic, pleased, and healthy again?",
+    "I'm feeling bored, mad, and sickly. Can you help me become excited, happy, and well once again?",
+    "I'm not feeling engaged, angry, and healthy. Can you offer any help to make me feel thrilled, pleased, and fit again?",
+    "I'm feeling indifferent, frustrated, and queasy. Can you provide any assistance in helping me become excited, content, and healthy again?",
+    "I'm feeling dull, irritated, and fearful. Can you help me become thrilled, happy, and in good health once again?",
+    "I'm feeling uninterested, displeased, and ailing. Can you help me become excited, content, and well once more?",
+    "I'm feeling bored, angry, and sick. Can you offer any assistance to make me feel thrilled, pleased, and healthy once again?", 
+    "I'm feeling indifferent, bothered, and unwell. Can you help me become excited, happy, and in good health again?",
+    "I'm feeling unengaged, vexed, and indisposed. Can you assist me in becoming thrilled, pleased, and fit once more?"
+
+]
+bored_angry_scary_list = [
+    "I am feeling bored, angry, and scared, help me become excited, pleased, and relaxed again.",
+    "I'm feeling uninterested, upset, and anxious. Can you help me become thrilled, content, and calm again?",
+    "I'm feeling disinterested, irate, and frightened. Can you assist me in becoming enthusiastic, pleased, and relaxed again?",
+    "I'm feeling bored, mad, and scared. Can you help me become excited, happy, and at ease once again?",
+    "I'm not feeling engaged, angry, and relaxed. Can you offer any help to make me feel thrilled, pleased, and calm again?",
+    "I'm feeling indifferent, frustrated, and tense. Can you provide any assistance in helping me become excited, content, and relaxed again?",
+    "I'm feeling dull, irritated, and fearful. Can you help me become thrilled, happy, and at ease once more?",
+    "I'm feeling uninterested, displeased, and anxious. Can you help me become excited, content, and relaxed once again?",
+    "I'm feeling bored, angry, and scared. Can you offer any assistance to make me feel thrilled, pleased, and at ease once again?",
+    "I'm feeling indifferent, bothered, and anxious. Can you help me become excited, happy, and relaxed again?",
+    "I'm feeling unengaged, vexed, and frightened. Can you assist me in becoming thrilled, pleased, and calm once more?"
+]
+bored_sick_scary_list = [
+    "I am feeling bored, sick, and scared, help me become excited, healthy, and relaxed again.",
+    "I'm feeling uninterested, unwell, and anxious. Can you help me become thrilled, healthy, and calm again?",
+    "I'm feeling disinterested, ill, and frightened. Can you assist me in becoming enthusiastic, fit, and relaxed again?",
+    "I'm feeling bored, queasy, and scared. Can you help me become excited, in good health, and at ease once again?",
+    "I'm not feeling engaged, sickly, and relaxed. Can you offer any help to make me feel thrilled, healthy, and calm again?",
+    "I'm feeling indifferent, nauseous, and tense. Can you provide any assistance in helping me become excited, well, and relaxed again?",
+    "I'm feeling dull, unwell, and fearful. Can you help me become thrilled, healthy, and at ease once more?",
+    "I'm feeling uninterested, ailing, and anxious. Can you help me become excited, in good health, and relaxed once again?",
+    "I'm feeling bored, sick, and scared. Can you offer any assistance to make me feel thrilled, healthy, and at ease once again?",
+    "I'm feeling indifferent, under the weather, and anxious. Can you help me become excited, fit, and relaxed again?",
+    "I'm feeling unengaged, poorly, and frightened. Can you assist me in becoming thrilled, healthy, and calm once more?"
+]
+angry_sick_scary_list = [
+    "I am feeling angry, sick, and scared, help me become pleased, healthy, and relaxed again.",
+    "I'm feeling furious, unwell, and anxious. Can you help me become content, in good health, and calm again?",
+    "I'm feeling irate, ill, and frightened. Can you assist me in becoming happy, fit, and relaxed again?",
+    "I'm feeling mad, queasy, and scared. Can you help me become pleased, healthy, and at ease once again?",
+    "I'm not feeling happy, sickly, and relaxed. Can you offer any help to make me feel content, healthy, and calm again?",
+    "I'm feeling frustrated, nauseous, and tense. Can you provide any assistance in helping me become pleased, well, and relaxed again?",
+    "I'm feeling angry, unwell, and fearful. Can you help me become pleased, healthy, and at ease once more?",
+    "I'm feeling upset, ailing, and anxious. Can you help me become content, in good health, and relaxed once again?",
+    "I'm feeling enraged, sick, and scared. Can you offer any assistance to make me feel pleased, healthy, and at ease once again?",
+    "I'm feeling annoyed, under the weather, and anxious. Can you help me become content, fit, and relaxed again?",
+    "I'm feeling irritated, poorly, and frightened. Can you assist me in becoming happy, healthy, and calm once more?"
+]
+
+bored_angry_sick_scary_list = [
+    "I am feeling bored, angry, sick, and scared, help me become excited, pleased, healthy, and relaxed again.",
+    "I'm feeling uninterested, furious, unwell, and anxious. Can you help me become thrilled, happy, in good health, and calm again?",
+    "I'm feeling disengaged, irate, ill, and frightened. Can you assist me in becoming enthusiastic, content, fit, and relaxed again?",
+    "I'm feeling dull, mad, queasy, and scared. Can you help me become excited, pleased, healthy, and at ease once again?",
+    "I'm not feeling thrilled, upset, sickly, and relaxed. Can you offer any help to make me feel excited, content, healthy, and calm again?",
+    "I'm feeling indifferent, frustrated, nauseous, and tense. Can you provide any assistance in helping me become thrilled, happy, well, and relaxed again?",
+    "I'm feeling bored, angry, unwell, and fearful. Can you help me become excited, pleased, healthy, and at ease once more?",
+    "I'm feeling disinterested, upset, ailing, and anxious. Can you help me become enthusiastic, content, in good health, and relaxed once again?",
+    "I'm feeling unenthusiastic, enraged, sick, and scared. Can you offer any assistance to make me feel thrilled, pleased, healthy, and at ease once again?",
+    "I'm feeling indifferent, annoyed, under the weather, and anxious. Can you help me become excited, content, fit, and relaxed again?",
+    "I'm feeling uninterested, irritated, poorly, and frightened. Can you assist me in becoming thrilled, happy, healthy, and calm once more?"
+]
+
+
+# one hot encoding with order: bored - angry - sick - scary
+question_dictionary = {
+    "0000": base_list,
+    "1000": bored_list,
+    "0100": angry_list,
+    "0010": sick_list,
+    "0001": scary_list,
+    "1100": bored_angry_list, 
+    "1010": bored_sick_list,
+    "1001": bored_scary_list,
+    "0110": angry_sick_list,
+    "0101": angry_scary_list,
+    "0011": sick_scary_list,
+    "1110": bored_angry_sick_list,
+    "1101": bored_angry_scary_list,
+    "1011": bored_sick_scary_list, 
+    "0111": angry_sick_scary_list,
+    "1111": bored_angry_sick_scary_list
+}
+
